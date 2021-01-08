1f63763f3570a2a98ba0b87b5e629a50c742f279
Tim Jang <timjang3@gmail.com>
2021-01-08 14:07:08 -0800

    accuracy and icon dynamics improvmements

--- contentScript.js
+++ contentScript.js
@@ -27,46 +27,56 @@ function update(){
 document.addEventListener("visibilitychange", function () {
     if(!document.hidden) {
         chrome.storage.local.set({'time': wpm});
+        chrome.runtime.sendMessage({
+            speed: wpm
+        });
     }
   }, false);
 
   window.addEventListener('load', (event) => {
     if(!document.hidden) {
         chrome.storage.local.set({'time': wpm});
+        chrome.runtime.sendMessage({
+            speed: wpm
+        });
     }
   });
 
 function myInput(event){
-    if(typing.length == 0){
-        masterStart = window.performance.now();
-        start = window.performance.now();
-    }
-    end = window.performance.now();
-    elapsed = end - start;
-    start = window.performance.now();
-
-    if(elapsed > 1000){
-        cutTime += elapsed;
-    }
-    
     var input = event.which || event.keyCode;
-    if(input != 8){
-        let typed = String.fromCharCode(input).toLowerCase();
-        typing = typing + typed;
-    }
-    else{
-        typing = typing.slice(0, typing.length - 1);
-    }
+    if(input != 13 && input != 17 && input != 18){
 
-    masterElapsed = window.performance.now() - masterStart;
-    finaltime = masterElapsed - cutTime;
-    wpm = Math.round((typing.length/(finaltime/1000/60))/5);
+        if(typing.length == 0){
+            masterStart = window.performance.now();
+            start = window.performance.now();
+        }
+        end = window.performance.now();
+        elapsed = end - start;
+        start = window.performance.now();
 
-    chrome.runtime.sendMessage({
-        speed: wpm
-    });
-    
-    //chrome.storage.local.set({'mtime': finaltime});
-    chrome.storage.local.set({'time': wpm});
-    //chrome.storage.local.set({'typed': typing});
+        if(elapsed > 1000){
+            cutTime += elapsed;
+        }
+        
+        
+        if(input != 8){
+            let typed = String.fromCharCode(input).toLowerCase();
+            typing = typing + typed;
+        }
+        else{
+            typing = typing.slice(0, typing.length - 1);
+        }
+
+        masterElapsed = window.performance.now() - masterStart;
+        finaltime = masterElapsed - cutTime;
+        wpm = Math.round((typing.length/(finaltime/1000/60))/5);
+
+        chrome.runtime.sendMessage({
+            speed: wpm
+        });
+        
+        //chrome.storage.local.set({'mtime': finaltime});
+        chrome.storage.local.set({'time': wpm});
+        //chrome.storage.local.set({'typed': typing});
+    }
 }
